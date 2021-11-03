d5901ef1fa8bc57444df84a75946e12a27499ec1
Griffin Tallant <griffin.tallant@gmail.com>
2021-11-03 11:53:37 -0600

    Use airtable instead of fs

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,34 +3,39 @@ import fs from 'fs';
 import path from 'path';
 import * as mm from 'music-metadata';
 
-const postsDirectory = path.join(process.cwd(), 'public/audio');
+
+const base = require('airtable').base('app8ATwuKjfAqLgYg');
 
 export async function listAudioFiles() {
-  const fileNames = fs.readdirSync(postsDirectory);
-  const formattedFileData = await Promise.all(fileNames.map(async fileName => {
-    const metadata = await mm.parseFile(`${postsDirectory}/${fileName}`, { duration: true });
-    const { title, artist } = metadata.common;
-    const { duration = 1 } = metadata.format;
-    const minutes = Math.floor(duration / 60);
-    const seconds = Math.floor(duration - minutes * 60);
+  const audioFiles: AudioFileMetadata[] = [];
+  const audioFilesTable = await base('Audio Files').select({
+    view: "Grid view"
+  });
+  await audioFilesTable.eachPage(async (records: any, fetchNextPage: any) =>{
+      await records.forEach(async (record: any) => {
+          const attachment = record.get('Attachments')[0];
+          const duration = record.get('Duration');
+          const minutes = Math.floor(duration / 60);
+          const seconds = Math.floor(duration - minutes * 60);
+          const durationStr = `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+          audioFiles.push({
+            id: record.id,
+            name: record.get('Name'),
+            artist: record.get('Artist'),
+            duration: durationStr,
+            url: attachment.url,
+          });
+      });
+      fetchNextPage();
 
-    const durationStr = `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-
-    return {
-      id: fileName.replace(/\.mp3$/, ''),
-      url: `/public/audio/${fileName}`,
-      duration: durationStr,
-      name: title,
-      artist: artist,
-    }
-  }));
-  return formattedFileData;
+  });
+  return audioFiles;
 }
 
 export type AudioFileMetadata = {
   id: string
   url: string
-  duration: number
+  duration: string
   name: string
   artist: string
 }
